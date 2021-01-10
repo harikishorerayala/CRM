47509a7d1c51df595b922e6445e4b9070bb70b2c
harikishorerayala <harikishorerayala@gmail.com>
2021-01-10 20:59:37 +0530

    practice structural and attribute directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,13 @@ export class AppComponent {
   title = 'Base';
 
 isAdded = false;
+
+contacts = [
+  {fname: 'hari', lname: 'kishore', number: 9448329329},
+  {fname: 'rama', lname: 'krishna', number: 1234567890},
+  {fname: 'krishna', lname: 'kishore', number: 7829321245}
+];
+
+superHero = 'Tony Stark';
+
 }
